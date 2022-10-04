0a6f9346b6d60a7e7555e68c870b00540a8b1f74
Jose Luis Sanchez Ortiz <jeluchez@gmail.com>
2022-10-03 22:44:28 -0500

    fix: remove share_files

--- terraform/registry/config/backend-config.tf
+++ terraform/registry/config/backend-config.tf
@@ -1,6 +1,5 @@
-bucket                  = "terraform-state"
-key                     = "terraform.tfstate"
-region                  = "us-east-1"
-encrypt                 = true
-dynamodb_table          = "terraform-locks"
-shared_credentials_file = "/Users/joseluis.sanchez/.aws/credentials"
+bucket         = "terraform-state"
+key            = "terraform.tfstate"
+region         = "us-east-1"
+encrypt        = true
+dynamodb_table = "terraform-locks"
