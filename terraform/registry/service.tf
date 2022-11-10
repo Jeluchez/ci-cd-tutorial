168395400a10408ac32b9969bdecc99ac678e49c
Jose Luis Sanchez Ortiz <jeluchez@gmail.com>
2022-11-09 20:37:06 -0500

    fix: change jsonEncode by <<DEFINITION

--- terraform/registry/service.tf
+++ terraform/registry/service.tf
@@ -7,23 +7,30 @@ resource "aws_ecs_task_definition" "service_task_fargate" {
   memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
-  container_definitions = jsonencode([{
-    image = "${var.account_id}.dkr.ecr.us-east-1.amazonaws.com/${var.service_name}:${var.image_tag}"
-    logConfiguration = {
-      logDriver = "awslogs",
-      options = {
-        awslogs-group         = "/aws/ecs/${var.service_name}"
-        awslogs-region        = var.aws_region
-        awslogs-create-group  = "true"
-        awslogs-stream-prefix = var.service_name
-      }
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "${var.service_name}",
+      "image": "${var.account_id}.dkr.ecr.us-east-1.amazonaws.com/${var.service_name}:${var.image_tag}",
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "/aws/ecs/${var.service_name}",
+          "awslogs-region": "${var.aws_region}",
+          "awslogs-create-group": "true",
+          "awslogs-stream-prefix": "${var.service_name}"
+        }
+      },
+      "portMappings": [
+        {
+          "protocol": "tcp",
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ]
     }
-    portMappings = [{
-      protocol      = "tcp"
-      containerPort = 3000
-      hostPort      = 3000
-    }] }
-  ])
+  ]
+  DEFINITION
 }
 
 resource "aws_ecs_service" "ecs_service" {
