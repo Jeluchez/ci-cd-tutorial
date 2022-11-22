906cabfce3b0a42b67eb10e6aa8cc74e35d604cd
Jose Luis Sanchez Ortiz <jeluchez@gmail.com>
2022-11-22 18:32:41 -0500

    chore: add certificate to https

--- terraform/registry/cert.tf
+++ terraform/registry/cert.tf
@@ -1,43 +1,44 @@
-
 resource "aws_route53_zone" "public" {
   name = var.domain_name
 }
-
-# create Certificate
 resource "aws_acm_certificate" "my_cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
-  lifecycle {
+    lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_route53_record" "cert_dns" {
+  for_each = {
+    for dvo in aws_acm_certificate.my_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   allow_overwrite = true
-  name =  tolist(aws_acm_certificate.my_cert.domain_validation_options)[0].resource_record_name
-  records = [tolist(aws_acm_certificate.my_cert.domain_validation_options)[0].resource_record_value]
-  type = tolist(aws_acm_certificate.my_cert.domain_validation_options)[0].resource_record_type
-  zone_id = aws_route53_zone.public.id
-  ttl = 60
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.public.zone_id
 }
 
+resource "aws_acm_certificate_validation" "my_cert_validation" {
+  certificate_arn         = aws_acm_certificate.my_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_dns : record.fqdn]
+}
 
-resource "aws_route53_record" "my_app" {
+resource "aws_route53_record" "main" {
   zone_id = aws_route53_zone.public.zone_id
-  name    = "${var.domain_name}.${aws_route53_zone.public.name}"
+  name    = var.domain_name
   type    = "A"
+
   alias {
-    name                   = aws_alb.my_app.dns_name
-    zone_id                = aws_alb.my_app.zone_id
-    evaluate_target_health = false
+    name                   = aws_lb.my_app.dns_name
+    zone_id                = aws_lb.my_app.zone_id
+    evaluate_target_health = true
   }
 }
-
-output "testing" {
-  value = "Test this demo code by going to https://${aws_route53_record.myapp.fqdn} and checking your have a valid SSL cert"
-}
-resource "aws_acm_certificate_validation" "my_cert_validation" {
-  certificate_arn = aws_acm_certificate.my_cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_dns.fqdn]
-}   
-
