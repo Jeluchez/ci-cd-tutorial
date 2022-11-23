b8ed2a2b9b5e4b140562b8e0f24db46e8e6860d1
Jose Luis Sanchez Ortiz <jeluchez@gmail.com>
2022-11-23 11:43:44 -0500

    chore: remove https

--- terraform/registry/lb.tf
+++ terraform/registry/lb.tf
@@ -38,11 +38,11 @@ resource "aws_lb_target_group" "my_app" {
 }
 
 resource "aws_lb" "my_app" {
-  name               = "myApp"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb.id]
-  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+  name                       = "myApp"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.lb.id]
+  subnets                    = [aws_subnet.public1.id, aws_subnet.public2.id]
   enable_deletion_protection = false
 }
 resource "aws_lb_listener" "my_app_http" {
@@ -50,34 +50,34 @@ resource "aws_lb_listener" "my_app_http" {
   port              = "80"
   protocol          = "HTTP"
 
-  # default_action {
-  #   type             = "forward"
-  #   target_group_arn = aws_lb_target_group.my_app.arn
-  # }
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-# listener for HTTPS
-resource "aws_lb_listener" "my_app_https" {
-  load_balancer_arn = aws_lb.my_app.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn = aws_acm_certificate_validation.my_cert_validation.certificate_arn
-
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.my_app.arn
   }
+  # default_action {
+  #   type = "redirect"
+
+  #   redirect {
+  #     port        = "443"
+  #     protocol    = "HTTPS"
+  #     status_code = "HTTP_301"
+  #   }
+  # }
 }
+
+# listener for HTTPS
+# resource "aws_lb_listener" "my_app_https" {
+#   load_balancer_arn = aws_lb.my_app.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = "arn:aws:acm:us-east-1:107748147623:certificate/3335a7cb-8b89-4330-8f66-39d3f0ba285b"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.my_app.arn
+#   }
+# }
 
 # resource "aws_lb_listener_certificate" "certificate_https" {
 #   listener_arn    = aws_lb_listener.my_app_https.arn
